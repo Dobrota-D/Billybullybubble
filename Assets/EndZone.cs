b9c51dc504b47a575d3f9cbbf020a50f7312ded6
MaximeJomain <72390612+MaximeJomain@users.noreply.github.com>
2025-01-25 22:35:15 +0100

    add main scene

---
+++ Assets/EndZone.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        throw new NotImplementedException();
+    }
+}
